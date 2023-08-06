617c92924e491f8bb093aedac6225b0c8952a7e4
Andrew Conway <arcgit@greatcactus.org>
2023-08-06 23:07:22 +1000

    File load button and ShangriLa support

--- WebContent/explain_assertions.js
+++ WebContent/explain_assertions.js
@@ -33,6 +33,8 @@ function interpret_input_formats(input) {
     } else {
         let Michelle = convert_from_Michelle_format(input);
         if (Michelle) return { format: "Michelle Blom RAIRE", contests: Michelle }
+        let ShangriLa = convert_from_ShangriLa_log_format(input);
+        if (ShangriLa) return { format: "ShangriLa log", contests: ShangriLa }
         return null;
     }
 }
@@ -44,34 +46,79 @@ function convert_from_Michelle_format(input) {
     for (const audit of input.audits) {
         const candidates = [audit.winner].concat(audit.eliminated);
         const metadata = { contest : audit.contest || "Unnamed contest", candidates : candidates };
-        const candidate_id_of_name = {}; // inverse of candidates
-        const num_candidates = candidates.length;
-        for (let i=0;i<num_candidates;i++) candidate_id_of_name[candidates[i]]=i;
-        const assertions = [];
-        for (const assertion of audit.assertions) {
-            const out = {
-                "winner" : candidate_id_of_name[assertion.winner],
-                "loser" : candidate_id_of_name[assertion.loser],
-            }
-            if (assertion.assertion_type==="IRV_ELIMINATION") {
-                out.type = "NEN";
-                out.continuing = [];
-                for (let i=0;i<num_candidates;i++) if (!assertion.already_eliminated.includes(candidates[i])) out.continuing.push(i);
-            } else if (assertion.assertion_type==="WINNER_ONLY") {
-                out.type = "NEB";
-            } else return null;
-            assertions.push({assertion: out});
-        }
+        const assertions = parseMichelleAssertions(audit.assertions,candidates);
+        if (assertions===null) return null;
         contests.push({
             metadata : metadata,
             solution : {
                 Ok : {
                     assertions : assertions,
                     winner:0,
-                    num_candidates:num_candidates
+                    num_candidates:candidates.length
                 }
             }
         })
     }
     return contests;
+}
+
+function parseMichelleAssertions(audit_assertions,candidates) {
+    const candidate_id_of_name = {}; // inverse of candidates
+    const num_candidates = candidates.length;
+    for (let i=0;i<num_candidates;i++) candidate_id_of_name[candidates[i]]=i;
+    const assertions = [];
+    for (const assertion of audit_assertions) {
+        const out = {
+            "winner" : candidate_id_of_name[assertion.winner],
+            "loser" : candidate_id_of_name[assertion.loser],
+        }
+        if (assertion.assertion_type==="IRV_ELIMINATION") {
+            out.type = "NEN";
+            out.continuing = [];
+            for (let i=0;i<num_candidates;i++) if (!assertion.already_eliminated.includes(candidates[i])) out.continuing.push(i);
+        } else if (assertion.assertion_type==="WINNER_ONLY") {
+            out.type = "NEB";
+        } else return null;
+        assertions.push({assertion: out});
+    }
+    return assertions;
+}
+
+function convert_from_ShangriLa_log_format(input) {
+    if (!input.hasOwnProperty("seed")) return null;
+    if (!input.hasOwnProperty("contests")) return null;
+    let contests = [];
+    for (const contest_id of Object.getOwnPropertyNames(input.contests)) {
+        const contest = input.contests[contest_id];
+        if (contest.n_winners!==1) continue;
+        if (!Array.isArray(contest.reported_winners)) continue;
+        if (contest.reported_winners.length!==1) continue;
+        if (contest.choice_function!=="IRV") continue;
+        const winner = contest.candidates.indexOf(contest.reported_winners[0]);
+        if (winner=== -1) continue;
+        const assertions=parseMichelleAssertions(contest.assertion_json,contest.candidates);
+        if (assertions===null) return null;
+        contests.push({
+            metadata : { contest : contest_id, candidates: contest.candidates },
+            solution : {
+                Ok : {
+                    assertions : assertions,
+                    winner: winner,
+                    num_candidates:contest.candidates.length
+                }
+            }
+        });
+    }
+    return contests;
+}
+
+
+window.onload = function () {
+    document.getElementById('InputFile').addEventListener('change', function() {
+        const filereader = new FileReader();
+        filereader.onload = () => {
+            document.getElementById("Input").value = filereader.result;
+        };
+        filereader.readAsText(this.files[0]);
+    });
 }
